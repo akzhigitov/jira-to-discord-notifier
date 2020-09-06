c6b6ced0d0ae0c6b1b82a0e36655121675e16a9d
Marat Akhziigitov <marat.akzhigitov@x5.ru>
2020-09-06 14:17:09 +0300

    Filters support

---
+++ main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/andygrunwald/go-jira"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+type Config struct {
+	JiraUrl      string `json:"jiraUrl"`
+	FilterId     int    `json:"filterId"`
+	WebHookUrl   string `json:"webHookUrl"`
+	JiraUsername string `json:"jiraUsername"`
+	JiraPassword string `json:"jiraPassword"`
+}
+
+type Message struct {
+	Embeds []Embed `json:"embeds"`
+}
+
+type Embed struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	URL         string `json:"url"`
+	Color       int    `json:"color"`
+	Author      Author `json:"author"`
+}
+
+type Author struct {
+	Name string `json:"name"`
+}
+
+var (
+	config     = &Config{}
+	jiraClient = &jira.Client{}
+)
+
+func createMessageFromIssues(issues []jira.Issue) (Message, bool) {
+	message := Message{}
+	for _, issue := range issues {
+		if issue.Fields.Watches.IsWatching {
+			continue
+		}
+
+		embed := Embed{
+			Title:       issue.Key,
+			Description: issue.Fields.Summary,
+			URL:         config.JiraUrl + "/browse/" + issue.Key,
+			Color:       15746887, //red
+			Author: Author{
+				Name: issue.Fields.Creator.Name},
+		}
+
+		message.Embeds = append(message.Embeds, embed)
+
+		_, err := jiraClient.Issue.AddWatcher(issue.ID, config.JiraUsername)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+	return message, len(message.Embeds) > 0
+}
+
+func main() {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	configPath := filepath.Join(dir, "config.json")
+	data, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		log.Fatalln("cant read config file:", err)
+	}
+
+	err = json.Unmarshal(data, config)
+	if err != nil {
+		log.Fatalln("cant parse config:", err)
+	}
+
+	tp := jira.BasicAuthTransport{
+		Username: config.JiraUsername,
+		Password: config.JiraPassword,
+	}
+
+	jiraClient, err := jira.NewClient(tp.Client(), config.JiraUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filter, _, err := jiraClient.Filter.Get(config.FilterId)
+	if err != nil {
+		log.Fatal(err)
+	}
+	issues, _, err := jiraClient.Issue.Search(filter.Jql, &jira.SearchOptions{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	message, ok := createMessageFromIssues(issues)
+
+	if ok {
+		bytesRepresentation, err := json.Marshal(message)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		log.Printf("Send %v", string(bytesRepresentation))
+
+		_, err = http.Post(config.WebHookUrl, "application/json", bytes.NewBuffer(bytesRepresentation))
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+}
